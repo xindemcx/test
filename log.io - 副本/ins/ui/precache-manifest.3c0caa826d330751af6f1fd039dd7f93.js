c10a0e742dbad9d6ec7135cceb356620e83dae27
xinde <zzllyh8@163.com>
2020-11-08 11:39:56 +0900

    test 4

---
+++ log.io - 副本/ins/ui/precache-manifest.3c0caa826d330751af6f1fd039dd7f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4632aec12412e9af6078afbc16e2a8f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "724605f373d1be55e86f",
+    "url": "/static/css/main.370c9611.chunk.css"
+  },
+  {
+    "revision": "fc88cd3fbe63cb134e2e",
+    "url": "/static/js/2.c72864ce.chunk.js"
+  },
+  {
+    "revision": "4ebd4c934480c725a320d9506119b042",
+    "url": "/static/js/2.c72864ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "724605f373d1be55e86f",
+    "url": "/static/js/main.f6dfeea9.chunk.js"
+  },
+  {
+    "revision": "c0a09a0b56a0c940f593",
+    "url": "/static/js/runtime-main.098509c1.js"
+  }
+]);
